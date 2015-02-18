bdcfe4b5eb6acee589bf5a52359745369254c15e
Daron Jones <daronalerts@gmail.com>
2015-02-18 18:56:30 +0000

    It was the request that was wrong

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,8 +3,9 @@ var mongoose = require('mongoose');
 
 module.exports = function() {
     var db = mongoose.connect(config.db);
-    //any file which uses this will require the model
+
+    //add the schema to the db (different from book)
     require('../app/models/user.server.model')(db); 
-    console.log(db);
+
     return db;
 };
