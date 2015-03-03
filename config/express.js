2f65f6721f4ac6a74165c652240ac5247e063681
Daron Jones <daronalerts@gmail.com>
2015-03-03 19:32:10 +0000

    passport

--- config/express.js
+++ config/express.js
@@ -5,7 +5,8 @@ var config = require('./config'),
     bodyParser = require('body-parser'),
     methodOverride = require('method-override'),
     session = require('express-session'),
-    passport = require('passport');
+    passport = require('passport'),
+    flash = require('flash');
 
 module.exports = function(){
     //Start with an express app
@@ -37,6 +38,7 @@ module.exports = function(){
     app.set('views', './app/views');
     app.set('view engine', 'ejs');
      
+    app.use(flash());
     app.use(passport.initialize());
     app.use(passport.session());
     
