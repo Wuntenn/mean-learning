9aadf3373344cb63ef8e2125f8d2e0f3a6be7c4d
Daron Jones <daronalerts@gmail.com>
2015-06-21 16:16:58 +0100

    Angular routes and services

---
+++ public/application.js
@@ -0,0 +1,13 @@
+'use strict';
+var mainApplicationModuleName = 'mean',
+    mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute', 'example']);
+
+mainApplicationModule.config(['$locationProvider',
+    function($locationProvider) {
+        $locationProvider.hashPrefix('!');
+    }
+]);
+
+angular.element(document).ready(function() {
+    angular.bootstrap(document, [mainApplicationModuleName]);
+});
---
+++ public/example/controllers/example.client.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+angular.module('example').controller('ExampleController', ['$scope', 
+    function($scope){
+        $scope.name = 'MEAN Application';
+    }
+]);
