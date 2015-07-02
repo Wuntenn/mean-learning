f0c05b95405a55e49f00dd08dca32c09d4407b7b
Daron Jones <daronalerts@gmail.com>
2015-07-02 20:45:31 +0100

    creating the angular views

---
+++ public/articles/controllers/articles.client.controller.js
@@ -0,0 +1,58 @@
+'use strict';
+angular.module('articles').controller('ArticlesController', ['$scope', '$routeParams', '$location', 'Authentication', 'Articles',
+    function($scope, $routeParams, $location, Authentication, Articles) {
+        $scope.authentication = Authentication;
+    }
+]);
+
+$scope.create = function() {
+    var article = new Article({
+        title: this.title,
+        content: this.content
+    });
+
+    article.$save(function(response) {
+        $location.path('articles/' + response._id);
+    }, function(errorResponse) {
+        $scope.error = errorResponse.data.message;
+    });
+};
+
+$scope.find = function() {
+    $scope.articles = Articles.query();
+};
+
+$scope.findOne = function() {
+    $scope.article = Articles.get({
+        articleID: $routeParams.articleId 
+    });
+};
+
+$scope.update = function() {
+    $scope.article.$update(function() {
+        $location.path('articles/', + $scope.article._id);
+    }, function(errorResponse) {
+        $scrope.error = errorResponse.data.message; 
+    });
+};
+$scope.delete = function() {
+    if (article) {
+        //Here you are on the list view so you had to specify which article you wanted to delete
+        //by calling the $scope.delete() with an article.
+        //the $remove takes care of business however we must manually delete the article from the
+        //list.
+        article.$remove(function() {
+            for (var i in $scope.articles) {
+                if ($scope.articles[i] === article) {
+                    $scope.articles.splice(i, 1);
+                }
+            }
+        });
+    } else {
+        //Here we are in the read view hence we don't call this method with the article as a parameter.
+        //In this case it just deletes the article in the $scope object.
+        $scope.article.$remove(function() {
+            $location.path('articles');
+        });
+    }
+};
